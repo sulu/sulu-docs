54feba9a380cea37344b1f57f79a23781b93c8d0
Johannes Moser HM <sofa@morgenstille.at>
2015-09-21 09:44:02 +0200

    added mr sulu and the theme

--- conf.py
+++ conf.py
@@ -271,3 +271,6 @@ texinfo_documents = [
 #     import sphinx_rtd_theme
 #     html_theme = 'sphinx_rtd_theme'
 #     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+sys.path.append(os.path.abspath('_themes'))
+html_theme_path = ['_themes']
+html_theme = 'kr'
